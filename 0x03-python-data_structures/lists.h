6c568dfe021acc4059ccc9e9e82f69c013a40382
Morgan <morganokeke@gmail.com>
2023-01-08 17:35:23 +0000

    function that adds 2 tuples.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -8,4 +8,5 @@ def print_reversed_list_integer(my_list=[]):
 def new_in_list(my_list, idx, element):
 def no_c(my_string):
 def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
 #endif /*LISTS_H*/
