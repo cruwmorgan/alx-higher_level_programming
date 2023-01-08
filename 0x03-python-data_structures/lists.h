c58bae1d4fdbe1ffee97cf1549b90c6a3f72f07c
Morgan <morganokeke@gmail.com>
2023-01-08 17:32:22 +0000

    function that prints a matrix of integers.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -7,4 +7,5 @@ def replace_in_list(my_list, idx, element):
 def print_reversed_list_integer(my_list=[]):
 def new_in_list(my_list, idx, element):
 def no_c(my_string):
+def print_matrix_integer(matrix=[[]]):
 #endif /*LISTS_H*/
