977ed790a98a872c54ed8e013f8e3bd0efd7af9c
Morgan <morganokeke@gmail.com>
2023-01-08 17:29:32 +0000

    function that removes all characters c and C from a string.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -6,4 +6,5 @@ def element_at(my_list, idx):
 def replace_in_list(my_list, idx, element):
 def print_reversed_list_integer(my_list=[]):
 def new_in_list(my_list, idx, element):
+def no_c(my_string):
 #endif /*LISTS_H*/
