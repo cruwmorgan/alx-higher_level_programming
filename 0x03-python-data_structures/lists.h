f5634207c1b14857d36ac7f6840d5296af20133d
Morgan <morganokeke@gmail.com>
2023-01-08 17:25:28 +0000

    function that replaces an element in a list at a specific position without modifying the original list (like in C).

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,4 +5,5 @@ def print_list_integer(my_list=[]):
 def element_at(my_list, idx):
 def replace_in_list(my_list, idx, element):
 def print_reversed_list_integer(my_list=[]):
+def new_in_list(my_list, idx, element):
 #endif /*LISTS_H*/
