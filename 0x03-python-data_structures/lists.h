9ef3e95b84270aa905d0b6426a97b6ead362fc2b
Morgan <morganokeke@gmail.com>
2023-01-09 10:49:12 +0000

    function that returns a tuple with the length of a string and its first character.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -9,4 +9,5 @@ def new_in_list(my_list, idx, element):
 def no_c(my_string):
 def print_matrix_integer(matrix=[[]]):
 def add_tuple(tuple_a=(), tuple_b=()):
+def multiple_returns(sentence):
 #endif /*LISTS_H*/
