59caef1879ae26fb7ef1474146a23d5a15d6a606
Morgan <morganokeke@gmail.com>
2023-01-10 01:42:05 +0000

    function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -13,4 +13,6 @@ def multiple_returns(sentence):
 def max_integer(my_list=[]):
 def divisible_by_2(my_list=[]):
 def delete_at(my_list=[], idx=0):
+int is_palindrome(listint_t **head);
+int check_pal(listint_t **head, listint_t *last);
 #endif /*LISTS_H*/
