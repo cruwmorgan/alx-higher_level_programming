9b9ea401ca4504701b7b04630c49b37c312d29c5
Morgan <morganokeke@gmail.com>
2023-01-10 01:24:50 +0000

    function that deletes the item at a specific position in a list.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -12,4 +12,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
 def multiple_returns(sentence):
 def max_integer(my_list=[]):
 def divisible_by_2(my_list=[]):
+def delete_at(my_list=[], idx=0):
 #endif /*LISTS_H*/
