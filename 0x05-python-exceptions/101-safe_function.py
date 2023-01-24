e2ddbdebb21d8dad3ab16f4e8cbdd0e2d5fb270f
Morgan <morganokeke@gmail.com>
2023-01-24 06:05:33 +0000

    function that executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_function(fct, *args):
+	try:
+		return (fct(*args))
+	except Exception as te:
+		stderr.write("Exception: {}\n".format(te))
+		return (None)
