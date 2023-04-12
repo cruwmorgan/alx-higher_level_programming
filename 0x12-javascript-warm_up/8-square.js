f4115429b187f47e326c1a9516ba093f038b8bc8
Morgan <morganokeke@gmail.com>
2023-04-12 03:47:33 -0700

    script that prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,9 +3,12 @@ const size = process.argv[2];
 if (!parseInt(size)) {
   console.log('Missing size');
 } else {
+  let string = '';
   for (let x = 0; x < size; x++) {
     for (let y = 0; y < size; y++) {
-      console.log('X');
+      string += 'X';
     }
+    string += '\n';
   }
+  console.log(string);
 }
