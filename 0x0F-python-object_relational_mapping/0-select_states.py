7248da41986ab041f4e09a3fbba35681955c2eff
Morgan <morganokeke@gmail.com>
2023-04-15 02:14:13 -0700

    script that lists all states from the database hbtn_0e_0_usa:

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat April 15 2023
+@author: Okeke Morgan
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: {} username password database_name".format(args[0]))
+        exit(1)
+    user = args[1]
+    password = args[2]
+    datab = args[3]
+    db = MySQLdb.connect(host='localhost', user=username,
+             passwd=password, db=datab, port=3306)
+     cur = db.cursor()
+     row_list = cur.execute('SELECT * FROM states ORDER BY states.id;')
+     rows = cur.fetchall()
+     for row in rows:
+        print(row)
+    cur.close()
+    db.close()
