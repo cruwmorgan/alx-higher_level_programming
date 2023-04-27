d81dbbb1a1fc56be2c4828b03c22bc90a86fadd5
Morgan <morganokeke@gmail.com>
2023-04-27 16:06:11 -0700

    Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL 0.0.0.0:5000/catch_me -w "You got me!"
+curl -s -o /dev/null 0.0.0.0:5000/catch_me -w "You got me!"
