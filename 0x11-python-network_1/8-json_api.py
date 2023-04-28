82ea8435666a5fd973fb5d50c08bcea8381c1a51
Morgan <morganokeke@gmail.com>
2023-04-28 05:07:14 -0700

    Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+from requests import post, codes
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) > 1:
+        q = {'q': argv[1]}
+    else:
+        q = {'q': ''}
+    response = post(url, data=q)
+    try:
+        obj = response.json()
+        if len(obj) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(obj['id'], obj['name']))
+    except:
+        print('Not a valid JSON')
