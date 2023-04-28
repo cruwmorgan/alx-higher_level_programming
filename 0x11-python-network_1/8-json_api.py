98311ae332e3d56c7bc03468152d82ad4234ef13
Morgan <morganokeke@gmail.com>
2023-04-28 05:20:43 -0700

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,7 +17,6 @@ if __name__ == "__main__":
         if len(obj) == 0:
             print('No result')
         else:
-            print('[{}]'.format(obj['id'], end="")
-            print(obj['name'])
+            print('[{}] {}'.format(obj['id'], obj['name'])
     except:
         print('Not a valid JSON')
