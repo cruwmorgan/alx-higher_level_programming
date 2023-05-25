e8adb378f2a5d62162b24e7bc0a76e1e995c16fe
cruwmorgan <morganokeke@gmail.com>
2023-05-25 01:22:20 -0700

    script that prints the number of movies where the character Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+let data;
+let count = 0;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    data = JSON.parse(body);
+    data.results.forEach(function (result) {
+      result.characters.forEach(function (character) {
+        const split = character.split('/');
+        if (split[split.length - 2] === '18') {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
